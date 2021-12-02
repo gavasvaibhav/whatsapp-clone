38549879ec3b03f151008f9d23990c20b9101da8
vaibhav <gavasvaibhav98@gmail.com>
2021-12-02 15:50:21 +0530

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyAwa-yGZTWK3UUPt5tL6knsZgyTwAYvNw8',
+    authDomain: 'wa-clone-tutorial-e3e2a.firebaseapp.com',
+    databaseURL: 'https://wa-clone-tutorial-e3e2a-default-rtdb.firebaseio.com',
+    projectId: 'wa-clone-tutorial-e3e2a',
+    storageBucket: 'wa-clone-tutorial-e3e2a.appspot.com',
+    messagingSenderId: '726402868259',
+    appId: '1:726402868259:web:4e65bb67390139fae1fd96',
+    measurementId: 'G-P8BGV5BS9X'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyAwa-yGZTWK3UUPt5tL6knsZgyTwAYvNw8',
+    authDomain: 'wa-clone-tutorial-e3e2a.firebaseapp.com',
+    databaseURL: 'https://wa-clone-tutorial-e3e2a-default-rtdb.firebaseio.com',
+    projectId: 'wa-clone-tutorial-e3e2a',
+    storageBucket: 'wa-clone-tutorial-e3e2a.appspot.com',
+    messagingSenderId: '726402868259',
+    appId: '1:726402868259:web:4e65bb67390139fae1fd96',
+    measurementId: 'G-P8BGV5BS9X'
+  }
 };
 
 /*
